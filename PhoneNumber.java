26b074026c272e4b516144d57b3e9eaf5697ff61
Piotr Milewski <piotr250tiger@gmail.com>
2019-03-05 23:45:04 -0500

    finished

---
+++ PhoneNumber.java
@@ -0,0 +1,15 @@
+public class PhoneNumber{
+
+    final Long phoneNumber;
+
+    public PhoneNumber(){
+        phoneNumber = 0;
+    }
+
+    public PhoneNumber(Long newPhoneNumber) throws IllegalArgumentException{
+        if (newPhoneNumber <= 9999999999L && newPhoneNumber >= 1000000000L)
+            phoneNumber = newPhoneNumber;
+        else
+            throw new IllegalArgumentException("Only 10-digit phone numbers are accepted");
+    }
+}
