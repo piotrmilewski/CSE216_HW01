afc19d2e77da19562676a1428b818d9756156a76
Piotr Milewski <piotr250tiger@gmail.com>
2019-03-07 20:35:16 -0500

    Finished first part

--- OldLandline.java
+++ OldLandline.java
@@ -3,7 +3,7 @@ public class OldLandline implements Phone {
     String name;
     boolean isBusy;
     PhoneNumber myNumber;
-    PhoneNumber caller;
+    Phone caller;
 
     public OldLandline(){
         name = "noName";
@@ -21,12 +21,14 @@ public class OldLandline implements Phone {
         return name;
     }
 
+    //if busy = print out correct error messages
+    //if not busy, set caller for both people
     public void call(Phone phone){
         if (phone.isBusy() || isBusy()){
             if (phone.isBusy())
                 System.out.println(getOwner() + " is unable to call " + phone.getOwner() + ". Line is currently busy.");
             else if (isBusy())
-                System.out.println(getOwner() + " cannot make calls while he is in another call.");
+                System.out.println(getOwner() + " cannot make calls while he/she is in another call.");
             else
                 System.out.println(getOwner() + " and " + phone.getOwner() + " are both currently in a call");
         }
@@ -34,28 +36,40 @@ public class OldLandline implements Phone {
             caller = phone;
             System.out.println(getOwner() + " is on the phone with " + phone.getOwner());
             phone.receive(this);
-        }
-            
+        }      
     }
 
     public void end(){
-
+        isBusy = false;
+        caller = null;
+        caller.receiveEndSignal(this);
     }
 
     public void receive(Phone from){
-
+        /* redundant, only add if receive is used elsewhere
+        if (isBusy() || from.isBusy()){
+            if (from.isBusy())
+                System.out.println(from.getOwner() + " is unable to call " + getOwner() + ". Line is currently busy.");
+            else if (isBusy())
+                System.out.println(getOwner() + " cannot take calls while he is in another call.");
+            else
+                System.out.println(getOwner() + " and " + from.getOwner() + " are both currently in a call");
+        }
+        */
+        caller = from;
     }
 
     public boolean isBusy(){
-        return true;
+        return isBusy;
     }
 
     public void receiveEndSignal(Phone from){
-
+        isBusy = false;
+        caller = null;
     }
 
     public PhoneNumber number(){
-        return new PhoneNumber();
+        return myNumber;
     }
 
 }
